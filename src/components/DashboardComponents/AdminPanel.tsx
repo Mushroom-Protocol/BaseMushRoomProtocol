7a3155196513ad562e600df4d009c158186bf346
Esteban <esteban@mushroomprotocol.io>
2023-12-28 14:47:02 -0300

    Dashboard Update

---
+++ src/components/DashboardComponents/AdminPanel.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  Center,
+  Grid,
+  GridItem,
+  Box,
+  Text,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel
+} from "@chakra-ui/react";
+
+const AdminPanel = () => {
+return (
+<Center>
+<Box>
+<Text fontSize="19px" color="#FFFFFF" bg="#FFFFFF">
+Mushroom Founders
+</Text>
+</Box>
+</Center>
+);
+};
+export default AdminPanel;
